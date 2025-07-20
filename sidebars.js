53e492af3eb9bd89a2bfe86eaf4886ca18357e69
Nahiyan Kamal <noyanayihan@gmail.com>
2025-07-20 12:08:34 +0200

    test message

--- sidebars.js
+++ sidebars.js
@@ -4,6 +4,8 @@ module.exports = {
     SystemDesign: ["backend/DesignSummary"],
     SoftwareDevProcess: ["backend/SoftDevPractices", "backend/DistributedTransaction"],
     JS: ["backend/Eventloop", "backend/AsyncVsThreading", "backend/MultiThreadVsMultiProcess"],
+    "Programming Languages": ["backend/ProgrammingLanguageTypes", "backend/ObjectOrientedProgramming"],
+    "Message Systems": ["backend/MessageQueue", "backend/Statelessness"],
     security: ["backend/jWT"],
   },
   Frontend: {
@@ -11,6 +13,7 @@ module.exports = {
   },
   Behavior: {
     Welcome: ["behavior/Introduction"],
+    "Interview Questions": ["behavior/BehavioralQuestions"],
     Recruiter: ["behavior/RecruiterGetToKnow"],
     Cultural: ["behavior/IntroductionCulture"],
   },
